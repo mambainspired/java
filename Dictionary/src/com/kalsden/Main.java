978d755b9b35e26c71c80abf3a4ff8e8c305f66a
mambainspired <kalyanchatterjee.ca@gmail.com>
2020-02-12 00:24:02 -0500

    Used HashMap to implement dictionary

--- Dictionary/src/com/kalsden/Main.java
+++ Dictionary/src/com/kalsden/Main.java
@@ -1,18 +1,16 @@
 package com.kalsden;
+import java.util.HashMap;
 
 /**
  * A simple Java class to implement a key-value data structure
  */
 
 public class Main {
-
     public static void main(String[] args) {
-        Dictionary dict = new Dictionary();
-
-        dict.addToDictionary("a", "Apple");
-        dict.addToDictionary("b", "Ball");
-        //dict.addToDictionary("c", "Cat");
-
-//        dict.print(dict);
+        HashMap<String, String> myDictionary = new HashMap<String, String>();
+        myDictionary.put("abysmal", "appalling");
+        myDictionary.put("acumen", "insightfulness: shrewdness shown by keen insight");
+        System.out.println(myDictionary);
+        System.out.println(myDictionary.get("acumen"));
     }
 }
